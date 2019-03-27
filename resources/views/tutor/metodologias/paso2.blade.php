dd70ed58a9310268512417a05bd50482d9ac2e83
Mauricio Manzano <maumanzano@unicauca.edu.co>
2019-03-27 00:47:05 -0500

    Se modifica el blade de crear practica paso 2: Equipo

--- resources/views/tutor/metodologias/paso2.blade.php
+++ resources/views/tutor/metodologias/paso2.blade.php
@@ -2,45 +2,119 @@
 
 @section('subtittle')
   <div class="title_left">
-    <h3> Creacion de Practica<small> paso 2</small> </h3>
+    <h3>Metodologias</h3>
   </div>
 @endsection
 
 @section('content')
-  <form class="form-horizontal form-label-left" action="" method="post">
-    <div class="x_content">
 
-      <table class="table">
-        <thead>
-          <tr>
-            <th>Codigo</th>
-            <th>Nombre</th>
-            <th>Apellido</th>
-            <th>Participacion</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>
+  <div class="">
+    <div class="page-title">
+      <div class="title_left">
+        <h3>Crear Metodologia: paso 2</h3>
+      </div>
+    </div>
+
+    <div class="clearfix"></div>
+
+    <div class="row">
+
+      <div class="col-md-12 col-sm-12 col-xs-12">
+        <div class="x_panel">
+          <div class="x_title">
+            <h2>Datos del equipo</h2>
+
+            <ul class="nav navbar-right panel_toolbox">
+              <li>
+                <a class="collapse-link">
+                  <i class="fa fa-chevron-up"></i>
+                </a>
+              </li>
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                  <i class="fa fa-wrench"></i>
+                </a>
+                <ul class="dropdown-menu" role="menu">
+                  <li>
+                    <a href="#">Settings 1</a>
+                  </li>
+                  <li>
+                    <a href="#">Settings 2</a>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <a class="close-link">
+                  <i class="fa fa-close"></i>
+                </a>
+              </li>
+            </ul>
+            <div class="clearfix"></div>
+          </div>
+          <div class="x_content">
+            <br/>
+            <form action="" method="POST" id="form_x" data-parsley-validate class="form-horizontal form-label-left">
+              {{ csrf_field() }}
               <div class="form-group">
+                <label class="control-label col-md-3 col-sm-3 col-xs-12" for="team_name">Nombre equipo <span class="required">*</span>
+                </label>
                 <div class="col-md-6 col-sm-6 col-xs-12">
-                  <div class="">
-                    <label>
-                      <input type="checkbox" class="js-switch" checked />
-                    </label>
-                  </div>
+                  <input type="text" id="team_name" required="required" class="form-control col-md-7 col-xs-12">
                 </div>
               </div>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+              <div class="form-group">
+                <label class="control-label col-md-3 col-sm-3 col-xs-12" for="description_tram">Descripcion <span class="required">*</span>
+                </label>
+                <div class="col-md-6 col-sm-6 col-xs-12">
+                  {!! Form::textArea('descripcion', null,['class' => 'form-control', 'required'])!!}
+                </div>
+              </div>
+              <div class="form-group">
+                <label class="control-label col-md-3 col-sm-3 col-xs-12" for="description_tram">Integrantes <span class="required"></span>
+                </label>
+                <table class="table">
+                  <thead>
+                    <tr>
+                      <th>Codigo</th>
+                      <th>Nombre</th>
+                      <th>Participacion</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <th scope="row">1</th>
+                      <td>Mark</td>
+                      <td>Otto</td>
+                      <td>
+                        <div class="form-group">
+                          <div class="col-md-6 col-sm-6 col-xs-12">
+                            <div class="">
+                              <label>
+                                <input type="checkbox" class="js-switch" checked />
+                              </label>
+                            </div>
+                          </div>
+                        </div>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
 
+              </div>
+
+              <div class="form-group">
+                <div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3">
+
+                  <button type="submit" class="btn btn-success">Siguiente</button>
+                </div>
+              </div>
+
+
+            </form>
+          </div>
+        </div>
+
+      </div>
     </div>
-  </form>
-
-</div>
+  </div>
 @endsection
